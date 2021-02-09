0abcaa584c7c1c25a96ea181a017cd5a3d502494
Ian Turner <ianturner88@gmail.com>
2021-02-09 09:39:01 -0500

    minor issues with formating 'rules of the game'

--- counter.js
+++ counter.js
@@ -8,16 +8,39 @@ class Counter {
   }
 
   increaseCount() {
-    this.count += 1;
+    // ensures the count does not exceed 5
+    if (counter.upperBoundary() == false) {
+      this.count += 1;
+    }
     counter.display();
   }
 
   decreaseCount() {
-    this.count -= 1;
+    // ensures the count does not exceed -5
+    if (counter.lowerBoundary() == false) {
+      this.count -= 1;
+    }
     counter.display();
   }
 
+  upperBoundary() {
+    // increment the count by 1
+    if (this.count > 4) {
+      return true;
+    }
+    return false;
+  }
+
+  lowerBoundary() {
+    // increment the count by -1
+    if (this.count < -4) {
+      return true;
+    }
+    return false;
+  }
+
   display() {
+    // output the result
     operand.innerText = `${this.count}`;
   }
 }
@@ -28,11 +51,9 @@ const counter = new Counter(
 );
 
 increaseCounter.addEventListener('click', (e) => {
-  console.log('Hello');
   counter.increaseCount();
 });
 
 decreaseCounter.addEventListener('click', (e) => {
-  console.log('Hello');
   counter.decreaseCount();
 });
